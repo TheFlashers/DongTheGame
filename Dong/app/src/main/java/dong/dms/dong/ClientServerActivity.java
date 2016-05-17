3a1208754a0e0c23c49767b06d9d31797cec61e4
Hiroto-N <Hiroto-N@users.noreply.github.com>
2016-05-17 17:50:06 +1200

    made device discoverable

--- Dong/app/src/main/java/dong/dms/dong/ClientServerActivity.java
+++ Dong/app/src/main/java/dong/dms/dong/ClientServerActivity.java
@@ -1,9 +1,11 @@
 package dong.dms.dong;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -26,6 +28,12 @@ public class ClientServerActivity extends Activity implements View.OnClickListen
         // Bind click events
         hostButton.setOnClickListener(this);
         joinButton.setOnClickListener(this);
+
+        Intent discoverableIntent
+                = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra
+                (BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,300); //5 min
+        startActivity(discoverableIntent);
     }
 
     @Override
@@ -36,16 +44,30 @@ public class ClientServerActivity extends Activity implements View.OnClickListen
             case R.id.joinButton : joinLogic(); break;
         }
 
-        Intent intent = new Intent(this, GameActivity.class);
+        //Intent intent = new Intent(this, GameActivity.class);
         // Maybe bundle some data before starting?
-        startActivity(intent);
+        //startActivity(intent);
     }
 
     private void hostLogic() {
         // TODO - Create session and await connection
+        //testing code for server and shit
+        ComNode cn = new DongServer();
+        Intent i = new Intent(this, ClientActivity.class);
+
+        i.putExtra("client", cn);
+        startActivity(i);
+
+
     }
 
     private void joinLogic() {
         // TODO - Join created session
+        //testing code for client and shit
+
+        ComNode chatNode = new DongClient();
+        Intent intent = new Intent(this, ClientActivity.class);
+        intent.putExtra("client", chatNode);
+        startActivity(intent);
     }
 }
